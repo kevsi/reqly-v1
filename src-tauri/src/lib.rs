7e06b073c9074fce54a7388c6a053513010c62bd
kevsi <1.38512935e+08+kevsi@users.noreply.github.com>
2026-08-13 17:34:56 +0000

    fix: stabilize artifact builds in CI

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -69,6 +69,7 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .manage(SharedClient {
             normal: http_client,
+            #[cfg(debug_assertions)]
             insecure: insecure_client,
         })
         .manage::<ManagedCaptureProxyState>(Arc::new(Mutex::new(
